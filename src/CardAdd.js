7563bb2a89a84f07e077ca4263d55272d7bca029
crown0205 <crown0205@gmail.com>
2022-03-26 09:32:17 +0900

    add page 마크업

--- src/CardAdd.js
+++ src/CardAdd.js
@@ -1,13 +1,78 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function CardAdd(){
+function CardAdd() {
+  const history = useHistory();
   return (
-    <>
     <div>
-      내가 있다~~
+      <div style={{ background: "#dfdfdf", padding: "10px" }}>
+        <h2>단어 추가</h2>
+        <form style={{ background: "#eee", margin: "50px" }}>
+          <div
+            style={{
+              background: "#aaa",
+              marginBottom: "20px",
+              width: "80%",
+              margin: "auto",
+            }}
+          >
+            <label
+              style={{ marginLeft: "10%", textAlign: "left", display: "block" }}
+            >
+              단어
+            </label>
+            <input style={{ width: "80%" }}></input>
+          </div>
+          <div
+            style={{
+              background: "#aaa",
+              marginBottom: "20px",
+              width: "80%",
+              margin: "auto",
+            }}
+          >
+            <label
+              style={{ marginLeft: "10%", textAlign: "left", display: "block" }}
+            >
+              단어
+            </label>
+            <input style={{ width: "80%" }}></input>
+          </div>
+          <div
+            style={{
+              background: "#aaa",
+              marginBottom: "20px",
+              width: "80%",
+              margin: "auto",
+            }}
+          >
+            <label
+              style={{ marginLeft: "10%", textAlign: "left", display: "block" }}
+            >
+              단어
+            </label>
+            <input style={{ width: "80%" }}></input>
+          </div>
+        </form>
+        <div
+          style={{
+            width: "200px",
+            height: "50px",
+            background: "#aaa",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "auto",
+          }}
+          onClick = {()=> {
+            history.goBack()
+          }}
+        >
+          저장하기
+        </div>
+      </div>
     </div>
-    </>
   );
 }
 
-export default CardAdd
+export default CardAdd;
