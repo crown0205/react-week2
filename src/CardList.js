8398932c073115b76b90ffd13653de879d945eb9
crown0205 <crown0205@gmail.com>
2022-03-27 14:47:12 +0900

    CardList style 정리

--- src/CardList.js
+++ src/CardList.js
@@ -1,76 +1,91 @@
 import React from "react";
+import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 
 function CardList() {
   const history = useHistory();
   return (
     <>
-      <div style={{ background: "#eee", padding: "10px" }}>
-        <div
-          style={{
-            background: "#dfdfdf",
-            borderRadius: "10px",
-            padding: "10px",
-            textAlign: "left",
-            position: "relative",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "24px",
-              fontWeight: "600",
-              marginBottom: "10px",
-            }}
-          >
-            title
-          </p>
-          <p
-            style={{
-              fontSize: "14px",
-              fontWeight: "400",
-              marginBottom: "10px",
-            }}
-          >
-            [title]
-          </p>
-          <p style={{ fontSize: "16px", fontWeight: "400", color: "#0984e3" }}>
-            예시
-          </p>
-
-          <div style={{ position: "absolute", top: "16px", right: "30px" }}>
+      <Container>
+        <div className="cardItem">
+          <p className="cardTitle">title</p>
+          <p className="cardtxt2">[title]</p>
+          <p className="cardtxt3">예시</p>
+          <div className="btnWrap">
             <span>check</span>
             <span>edit</span>
             <span>add</span>
           </div>
         </div>
-        <div
-          style={{
-            background: "#aaa",
-            position: "fixed",
-            bottom: "30px",
-            right: "30px",
-            display: "inline-block",
-            width: "50px",
-            height: "50px",
-            borderRadius: "50%",
-          }}
-          onClick={()=>{
-            history.push('/add')
-          }}
-        >
-          <span
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+        <div className="upBtn" onClick={() => {
+            history.push("/add");
+          }}>
             <p>+</p>
-          </span>
         </div>
-      </div>
+      </Container>
     </>
   );
 }
 
+const Container = styled.div`
+  background-color: #eee;
+  padding: 10px;
+
+  .cardItem {
+    background-color: #dfdfdf;
+    border-radius: 10px;
+    padding: 10px;
+    text-align: left;
+    position: relative;
+    height: 130px;
+    margin-bottom: 10px;
+  }
+
+  .cardTitle {
+    font-size: 24px;
+    font-weight: 600;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .cardtxt2 {
+      font-size: 14px;
+      font-weight: 400;
+      margin-bottom: 10px;
+  }
+  .cardtxt3 {
+    font-size: 16px;
+    font-weight: 400; 
+    color: #0984e3;
+  }
+
+  .btnWrap {
+    position: absolute;
+    top: 24px;
+    right: 30px;
+
+    span {
+      margin-right: 14px;
+    }
+  }
+
+.upBtn {
+  background-color: #aaa;
+  position:fixed;
+  bottom: 30px;
+  right: 30px;
+  display: flex;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+
+    p {
+      font-size: 26px;
+      font-weight: 700;
+    }
+
+}
+`;
 export default CardList;
